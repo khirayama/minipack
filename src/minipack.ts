5376227792548df6243f332b3f01d9aa744a32be
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-07-13 09:28:40 +0900

    Define buildModuleString

--- src/minipack.ts
+++ src/minipack.ts
@@ -1,7 +1,59 @@
-export function createGraph(entryPoint: string): string {
-  return entryPoint;
+export interface IModule {
+  id: any;
+  mapping: any;
+  code: any;
 }
 
-export function bundle(graph: string): string {
-  return graph;
+export function createGraph(entryPoint: string): IModule[] {
+  return [{
+    id: 1,
+    mapping: 'mapping',
+    code: 'code',
+  }];
+}
+
+export function buildModuleString(graph: IModule[]): string {
+  let moduleString: string = '';
+
+  graph.forEach((mod: IModule) => {
+    moduleString += `${mod.id}: [
+      function(require, module, exports) {
+        ${mod.code}
+      },
+      ${JSON.stringify(mod.mapping)}
+    ],`;
+  });
+
+  return moduleString;
+}
+
+export function bundle(graph: IModule[]): string {
+  const moduleString: string = buildModuleString(graph);
+
+  return `
+    (function(modules) {
+      function require(id) {
+        const mod = modules[id];
+        if (!mod) {
+          return;
+        }
+
+        const [fn, mapping] = modules[id];
+
+        function localRequire(name) {
+          return require(mapping[name]);
+        }
+
+        const module = {
+          exports: {},
+        };
+
+        fn(localRequire, module, module.exports);
+
+        return module.exports;
+      }
+
+      require(0);
+    })({${moduleString}});
+  `;
 }
--- test/minipack.ts
+++ test/minipack.ts
@@ -1,22 +1,95 @@
 import * as assert from 'power-assert';
 
-import { bundle, createGraph } from 'minipack';
+import { buildModuleString, bundle, createGraph, IModule } from 'minipack';
 
-const entryPoint: string = './example/entry.js';
+const sampleGraph1: IModule[] = [{
+  id: 1,
+  mapping: {
+    key1: 'val1',
+    key2: 'val2',
+  },
+  code: 'console.log("Call function");',
+}];
+
+
+function minifyForCompare(str: string): string {
+  const lines: string[] = str.split('\n');
+  let result: string = '';
+  result += lines.map((line: string) => line.trim()).join('');
+
+  return result;
+}
+
+describe('minifyForCompare', () => {
+  it('Simple sample', () => {
+    assert.equal(minifyForCompare('abc\n  defg'), 'abcdefg');
+  });
+});
 
 describe('minipack', () => {
-  describe('bundle', () => {
-  });
 
   describe('createGraph', () => {
   });
 
-  it('result', () => {
-    const graph: string = createGraph(entryPoint);
-    const result: string = bundle(graph);
+  describe('buildModuleString', () => {
+    it('Single module graph', () => {
+      const mod1: IModule = sampleGraph1[0];
+      const expected: string = `
+        ${mod1.id}: [function(require, module, exports) {
+          ${mod1.code}
+        },${JSON.stringify(mod1.mapping)}],
+      `;
+      const actual: string = buildModuleString(sampleGraph1);
 
-    // tslint:disable-next-line:no-console
-    console.log('result: ', result);
-    assert.equal(result, './example/entry.js');
+      assert.equal(minifyForCompare(actual), minifyForCompare(expected));
+    });
+  });
+
+  describe('bundle', () => {
+    it('Single module graph', () => {
+      const expected: string = `
+        (function(modules) {
+          function require(id) {
+            const mod = modules[id];
+            if (!mod) {
+              return;
+            }
+
+            const [fn, mapping] = modules[id];
+
+            function localRequire(name) {
+              return require(mapping[name]);
+            }
+
+            const module = {
+              exports: {},
+            };
+
+            fn(localRequire, module, module.exports);
+
+            return module.exports;
+          }
+
+          require(0);
+        })({${buildModuleString(sampleGraph1)}});
+      `;
+      const actual: string = bundle(sampleGraph1);
+
+      // tslint:disable-next-line:no-eval
+      assert.doesNotThrow(() => eval(actual));
+      assert.equal(minifyForCompare(actual), minifyForCompare(expected));
+    });
+  });
+
+  describe('End to End', () => {
+    it('runable', () => {
+      // const entryPoint: string = './example/entry.js';
+      // const graph: IModule[] = createGraph(entryPoint);
+      // const result: string = bundle(graph);
+      //
+      // // tslint:disable-next-line:no-console
+      // console.log('result: ', result);
+      // assert.equal(result, './example/entry.js');
+    });
   });
 });
